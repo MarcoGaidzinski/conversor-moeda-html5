938909b385019340d9394cdab454207a40093b5a
Marco Aurélio Gaidzinski <gaidzinski@gmail.com>
2019-08-22 11:33:02 +0000

    Inclusão Data

--- main.js
+++ main.js
@@ -7,9 +7,25 @@ function chamarAPI(){
 function callback(resultado){
 	// console.log(resultado);
 	
+	var sMoedaOrigem = $("#moeda_origem").val();
 	var sMoedaDestino = $("#moeda_destino").val();
-	var fValor = resultado.rates[sMoedaDestino];
 	
+	if(resultado.base != sMoedaOrigem){
+		// console.log("diferente");
+		var fValorOrigem = resultado.rates[sMoedaOrigem];
+		var fValorDestino = resultado.rates[sMoedaDestino];
+		if (fValorOrigem != 0){
+			var fValor = fValorDestino / fValorOrigem;
+		}
+	}
+	else{
+		var fValor = resultado.rates[sMoedaDestino];
+	}
+	
+	var sDataConsulta = resultado.date;
+	sDataConsulta = sDataConsulta.substring(8, 10) + "/" + sDataConsulta.substring(5, 7) + "/" + sDataConsulta.substring(0, 4);
+
+	$("#data_cotacao").text(sDataConsulta);
 	$("#valor_destino").text(fValor);
 	$("#moeda_destino_celula").text(sMoedaDestino);
 }
