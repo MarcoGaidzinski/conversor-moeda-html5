bf78dc8d447a1974a65088537dbec18a0cf83565
Marco Aurélio Gaidzinski <gaidzinski@gmail.com>
2019-08-24 01:30:38 +0000

    Remocao de funcoes globais

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-function chamarAPI(){
-	console.log("Chamando API");
-	const endpoint = "api/latest";
-	var oParametros = {
-		base: $("#moeda_origem").val()
-	};
-	$.get(endpoint, oParametros, callback );
-}
+const ovly = { };
 
-function callback(resultado){
-	// console.log(resultado);
+ovly.conversor = {
+	versao: "1",
+	chamarAPI: function(){
+		console.log("Chamando API");
+		const endpoint = "api/latest";
+		var oParametros = {
+			base: $("#moeda_origem").val()
+		};
+		$.get(endpoint, oParametros, this.callback );
+	},
+	callback: function(resultado){
+		// var sMoedaOrigem = $("#moeda_origem").val();
+		var sMoedaDestino = $("#moeda_destino").val();
+		var fValor = resultado.rates[sMoedaDestino];
 	
-	var sMoedaOrigem = $("#moeda_origem").val();
-	var sMoedaDestino = $("#moeda_destino").val();
-	var fValor = resultado.rates[sMoedaDestino];
-
-	var sDataConsulta = resultado.date;
-	sDataConsulta = sDataConsulta.substring(8, 10) + "/" + sDataConsulta.substring(5, 7) + "/" + sDataConsulta.substring(0, 4);
-
-	$("#data_cotacao").text(sDataConsulta);
-	$("#valor_destino").text(fValor);
-	$("#moeda_destino_celula").text(sMoedaDestino);
+		// var sDataConsulta = resultado.date;
+		// sDataConsulta = sDataConsulta.substring(8, 10) + "/" + sDataConsulta.substring(5, 7) + "/" + sDataConsulta.substring(0, 4);
+	
+		// $("#data_cotacao").text(sDataConsulta);
+		$("#valor_destino").text(fValor);
+		$("#moeda_destino_celula").text(sMoedaDestino);
+	}
 }
