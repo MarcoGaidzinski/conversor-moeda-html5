f5fea52df1c46b049cef8fba7a1a0d9c1239bdda
Marco Aurélio Gaidzinski <gaidzinski@gmail.com>
2019-08-27 19:08:55 +0000

    consulta com data ou sem data

--- main.js
+++ main.js
@@ -2,25 +2,44 @@ const ovly = { };
 
 ovly.conversor = {
 	versao: "1",
+	// https://api.exchangeratesapi.io/latest
 	chamarAPI: function(){
 		console.log("Chamando API");
-		const endpoint = "api/latest";
+		var data_consulta = $("#data_consulta").val();
+		const endpoint = "api/" + data_consulta;
+		const endpoint_latest = "api/latest";
 		var oParametros = {
 			base: $("#moeda_origem").val(),
-			// date: $("#data_consulta").val(),
+		//  date: $("#data_consulta").val(), 
 		};
-		$.get(endpoint, oParametros, this.callback );
+		// $.get(endpoint, oParametros, this.callback );
+		if( data_consulta == ""){
+			$.get(endpoint_latest, oParametros, this.callback);
+		}
+		else{
+			var jqxhr = $.get(endpoint, oParametros, ovly.conversor.callback) 
+			  .done(function() {
+			    console.log("Sucesso");
+			  })
+			  .fail(function() {
+				console.log("Erro");
+				$.get(endpoint_latest, oParametros, ovly.conversor.callback);
+			  })
+			  .always(function() {
+			    console.log("Finished");
+			  });			
+		};
 	},
 	callback: function(resultado){
 		// var sMoedaOrigem = $("#moeda_origem").val();
 		var sMoedaDestino = $("#moeda_destino").val();
 		var fValor = resultado.rates[sMoedaDestino];
 	
-		// var sDataConsulta = resultado.date;
-		// sDataConsulta = sDataConsulta.substring(8, 10) + "/" + sDataConsulta.substring(5, 7) + "/" + sDataConsulta.substring(0, 4);
+		var sDataConsulta = resultado.date;
+		sDataConsulta = sDataConsulta.substring(8, 10) + "/" + sDataConsulta.substring(5, 7) + "/" + sDataConsulta.substring(0, 4);
 	
-		// $("#data_cotacao").text(sDataConsulta);
+		$("#data_cotacao").text(sDataConsulta);
 		$("#valor_destino").text(fValor);
 		$("#moeda_destino_celula").text(sMoedaDestino);
-	}
+	},
 }
